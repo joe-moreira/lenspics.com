0c297b2b18f76e6acefb8d5e620caf42a69b874a
Jose Moreira <jgmoreira@gmail.com>
2020-11-23 12:09:16 -0800

    added views

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/views"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
 		panic(err)
 	}
 }
-func faq(w http.ResponseWriter, r *http.Request) {
+
+/*func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "Any question?.")
 }
-
+*/
 /*func notfound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
@@ -36,8 +38,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 */
 func main() {
 	//	var h http.Handler = http.HandlerFunc(notfound)
-	var err error
-	homeTemplate, err = template.ParseFiles(
+	//var err error
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
+	/*homeTemplate, err = template.ParseFiles(
 		"views/home.gohtml",
 		"views/layouts/footer.gohtml")
 	if err != nil {
@@ -48,11 +53,11 @@ func main() {
 		"views/layouts/footer.gohtml")
 	if err != nil {
 		panic(err)
-	}
+	}*/
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
+	//	r.HandleFunc("/faq", faq)
 	//	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
